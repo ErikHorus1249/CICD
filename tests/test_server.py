4565c3efd0ff7640f62e6bdea4490cb717b44cdf
ErikHorus1249 <tuananh1421999@gmail.com>
2022-04-22 15:24:35 +0700

    delete requirements

---
+++ tests/test_server.py
@@ -0,0 +1,11 @@
+import os 
+
+def test_number():
+    assert 1 == 1 
+    assert 2 == 2 
+    assert 2 == 2 
+    assert 3 == 3 
+    
+def test_ping():
+    #and then check the response...
+    assert os.system("ping -c 1 google.com") == 0
